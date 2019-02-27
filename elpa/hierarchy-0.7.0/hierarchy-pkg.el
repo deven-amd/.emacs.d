37385647ba1b46af3a130145aaf2f1a314018fbc
Deven Desai <deven.desai.amd@gmail.com>
2019-02-27 10:26:15 -0500

    deven-dev01 update

---
+++ elpa/hierarchy-0.7.0/hierarchy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hierarchy" "0.7.0" "Library to create and display hierarchy structures" '((emacs "25.1")) :commit "4ab1372c252847c316f8978a81e2fe92ff79579e" :authors '(("Damien Cassou" . "damien@cassou.me")) :maintainer '("Damien Cassou" . "damien@cassou.me") :url "https://github.com/DamienCassou/hierarchy")
